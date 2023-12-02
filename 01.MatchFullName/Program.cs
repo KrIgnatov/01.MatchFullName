97fe1806daafde7b710202791b33e76755f16087
KrIgnatov <124387062+KrIgnatov@users.noreply.github.com>
2023-12-02 16:44:16 +0200

    SolutionExc1&2

--- 01.MatchFullName/Program.cs
+++ 01.MatchFullName/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string inputText = Console.ReadLine();
+
+        string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+        MatchCollection matches = Regex.Matches(inputText, pattern);
+
+        foreach (Match match in matches)
+        {
+            Console.Write($"{match.Value} ");
+        }
+    }
+}
---
+++ 02.MatcPhoneNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿    using System;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"\+359( |-)2\1\d{3}( |-)\d{4}\b";
+            Regex regex = new Regex(pattern);
+
+
+            MatchCollection matches = regex.Matches(input);
+
+            Console.WriteLine(string.Join(", ", matches));
+        }
+    }
